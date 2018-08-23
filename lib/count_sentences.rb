4a2dd96053ce230239b6ab29517b10ad8ba87a24
Robert Snyder <r.snyder49@gmail.com>
2018-08-23 20:09:15 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,8 @@ class String
   end
 
   def count_sentences
-    sentence_array = self.split(/[.?!]/)
+    sentence_array = self.split(/[.?!,]/)
     sentence_array.delete_if{|ele| ele.empty?}
-    sentence_array.length
+    sentence_array.count
   end
 end
