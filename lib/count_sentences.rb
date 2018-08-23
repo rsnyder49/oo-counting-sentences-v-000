0cc3366875ca84bd7a0b6f5e7fc1e6f0ec1e252d
Robert Snyder <r.snyder49@gmail.com>
2018-08-23 20:17:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    str_array = self.split(/[.?!]/)
+    str_array.delete_if{|ele| ele == ""}
+    str_array.count
   end
 end
